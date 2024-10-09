6f096ecf567d861cbf5134925c18dbeaea151f8d
thomasarivera <thomas5info@gmail.com>
2024-10-08 19:08:12 -1000

    added text

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,16 +5,30 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+    <Row className="align-middle text-center my-5">
+      <Col className="justify-content-center">
+        <h1>Noble</h1>
+        <h3>Prepared for Tomorrow</h3>
       </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
+    </Row>
+    <Row className="align-middle text-center my-5">
+      <Col className="text-start">
+        <p>Ask health-related questions anonymously.
+        </p>
       </Col>
-
+      <Col>
+        <p>Image</p>
+      </Col>
+    </Row>
+    <Row className="align-middle text-center my-5">
+      <Col>
+        <p>Image</p>
+      </Col>
+      <Col className="text-start">
+        <p>Our interactive Question Compass guides you step-by-step through common concerns, providing personalized advice
+          based on your responses. Whether it&apos;s a minor query or a more complex issue, you&apos;ll get the guidance you need.
+        </p>
+      </Col>
     </Row>
   </Container>
 );
