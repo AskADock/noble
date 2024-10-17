6b5cacad608e341e8d8f08fb751f84d7d5fee8bd
thomasarivera <thomas5info@gmail.com>
2024-10-16 15:54:51 -1000

    update initial insert of data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,28 +1,41 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/StuffCollection';
+// import { Stuffs } from '../../api/stuff/StuffCollection';
 import { Categories } from '../../api/category/CategoryCollection';
 import { Questions } from '../../api/question/QuestionCollection';
 import { FAQ } from '../../api/faq/FAQCollection';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.define(data);
-}
+// function addData(data) {
+//   console.log(`  Adding: ${data.name} (${data.owner})`);
+//   Stuffs.define(data);
+// }
 
+// Initialize the database with a default category document.
 function addCategory(data) {
   console.log(`  Adding: ${data.category}`);
   Categories.define(data);
 }
 
-// Initialize the StuffsCollection if empty.
-if (Stuffs.count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
+// Initialize the database with a default question document.
+function addQuestion(data) {
+  console.log(`  Adding: ${data.question}`);
+  Questions.define(data);
 }
+
+// Initialize the database with a default FAQ document.
+function addFAQ(data) {
+  console.log(`  Adding: ${data.question}`);
+  FAQ.define(data);
+}
+
+// Initialize the StuffsCollection if empty.
+// if (Stuffs.count() === 0) {
+//   if (Meteor.settings.defaultData) {
+//     console.log('Creating default data.');
+//     Meteor.settings.defaultData.forEach(data => addData(data));
+//   }
+// }
 
 // Initialize the CategoriesCollection if empty
 if (Categories.count() === 0) {
@@ -36,7 +49,7 @@ if (Categories.count() === 0) {
 if (Questions.count() === 0) {
   if (Meteor.settings.defaultQuestions) {
     console.log('Creating default questions.');
-    Meteor.settings.defaultQuestions.forEach(data => Questions.define(data));
+    Meteor.settings.defaultQuestions.forEach(data => addQuestion(data));
   }
 }
 
@@ -44,6 +57,6 @@ if (Questions.count() === 0) {
 if (FAQ.count() === 0) {
   if (Meteor.settings.defaultFAQ) {
     console.log('Creating default FAQs.');
-    Meteor.settings.defaultFAQ.forEach(data => FAQ.define(data));
+    Meteor.settings.defaultFAQ.forEach(data => addFAQ(data));
   }
 }
