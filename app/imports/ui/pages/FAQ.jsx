1a48ba80d663ed6e1981710fcf3e739d1bcd7df6
thomasarivera <thomas5info@gmail.com>
2024-10-19 19:02:30 -1000

    removed old code

--- app/imports/ui/pages/FAQ.jsx
+++ app/imports/ui/pages/FAQ.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card, Form, Button, Pagination, Accordion, Badge } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { FAQ } from '../../api/faq/FAQCollection';
 import { Categories } from '../../api/category/CategoryCollection';
@@ -33,51 +33,6 @@ const FrequentlyAskedQuestions = () => {
           <Row>
             <FAQFilter faq={faq} categories={categories} />
           </Row>
-          {/*<Row className="justify-content-center">*/}
-          {/*  <Col className="col-9">*/}
-          {/*    <Row>*/}
-          {/*      <Col className="col-8">*/}
-          {/*        <Form.Control type="text" placeholder="I want to know..." />*/}
-          {/*      </Col>*/}
-          {/*      <Col className="col-3">*/}
-          {/*        <Form.Select>*/}
-          {/*          <option>All Categories</option>*/}
-          {/*          {categories.map((category) => (*/}
-          {/*            <option key={category._id}>{category.category}</option>*/}
-          {/*          ))}*/}
-          {/*        </Form.Select>*/}
-          {/*      </Col>*/}
-          {/*    </Row>*/}
-          {/*  </Col>*/}
-          {/*</Row>*/}
-          {/*<Row>*/}
-          {/*  <Col>*/}
-          {/*    {faq.map((question) => (*/}
-          {/*      <Card className="my-3" key={question._id}>*/}
-          {/*        <Accordion>*/}
-          {/*          <Accordion.Item eventKey={question._id} className="p-2">*/}
-          {/*            <Badge bg="primary" className="ms-auto">{question.category}</Badge>*/}
-          {/*            <Accordion.Header>*/}
-          {/*              <h5>{question.question}</h5>*/}
-          {/*            </Accordion.Header>*/}
-          {/*            <Accordion.Body>{question.answer}</Accordion.Body>*/}
-          {/*          </Accordion.Item>*/}
-          {/*        </Accordion>*/}
-          {/*      </Card>*/}
-          {/*    ))}*/}
-          {/*  </Col>*/}
-          {/*</Row>*/}
-          {/*<Row className="py-3">*/}
-          {/*  <Col className="d-flex justify-content-center">*/}
-          {/*    <Pagination>*/}
-          {/*      <Pagination.Prev />*/}
-          {/*      <Pagination.Item>{1}</Pagination.Item>*/}
-          {/*      <Pagination.Item>{2}</Pagination.Item>*/}
-          {/*      <Pagination.Item>{3}</Pagination.Item>*/}
-          {/*      <Pagination.Next />*/}
-          {/*    </Pagination>*/}
-          {/*  </Col>*/}
-          {/*</Row>*/}
           <Row className="justify-content-center text-center py-3">
             <Col className="col-4">
               <Card className="rounded-3 p-3">
