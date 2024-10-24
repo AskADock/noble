b5945704dd0f9e1565fa4fbde10bdb2f5f034694
Brandon Underwood <103099466+branndonU@users.noreply.github.com>
2024-10-23 16:58:23 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -12,17 +12,17 @@ const Landing = () => (
       </Col>
     </Row>
     <Row className="mt-2 mb-5 py-5 g-3 justify-content-center text-shadow">
-      <Col xs="auto">
+      <Col xs={12} sm="auto">
         <Button className="rounded-3" variant="outline-light" size="lg" href="/faq">
           FAQ
         </Button>
       </Col>
-      <Col xs="auto">
+      <Col xs={12} sm="auto">
         <Button className="rounded-3" variant="outline-light" size="lg" href="/">
           Question Compass
         </Button>
       </Col>
-      <Col xs="auto">
+      <Col xs={12} sm="auto">
         <Button className="rounded-3" variant="outline-light" size="lg" href="/">
           Ask A Doc
         </Button>
@@ -32,10 +32,10 @@ const Landing = () => (
       <Col className="col-10">
         <Card className="rounded-3 p-5 m-5">
           <Row className="text-center">
-            <Col className="col-6">
-              <img src="/images/askadoc.png" alt="Logo" width="100" height="100" />
+            <Col xs={12} md={6}>
+              <img src="/images/FAQ.png" alt="Logo" width="100" height="100" />
             </Col>
-            <Col className="col-6">
+            <Col xs={12} md={6}>
               <h2>Ask health-related questions anonymously</h2>
               <p>Whether it&apos;s a minor query or a more complex issue, you&apos;ll find the guidance you need.</p>
             </Col>
@@ -47,12 +47,12 @@ const Landing = () => (
       <Col className="col-10">
         <Card className="rounded-3 p-5 m-5">
           <Row className="text-center">
-            <Col className="col-6">
+            <Col xs={12} md={6}>
               <h2>Question Compass</h2>
               <p>Find the right path to your health answers</p>
             </Col>
-            <Col className="col-6">
-              <img src="/images/compasss.png" alt="Logo" width="100" height="100" />
+            <Col xs={12} md={6}>
+              <img src="/images/Compass.png" alt="Logo" width="100" height="100" />
             </Col>
           </Row>
         </Card>
@@ -62,29 +62,12 @@ const Landing = () => (
       <Col className="col-10">
         <Card className="rounded-3 p-5 m-5">
           <Row className="text-center">
-            <Col className="col-6">
-              <img src="/images/askadoc.png" alt="Logo" width="100" height="100" />
+            <Col xs={12} md={6}>
+              <img src="/images/AskADoc.png" alt="Logo" width="100" height="100" />
             </Col>
-            <Col className="col-6">
+            <Col xs={12} md={6}>
               <h2>Ask A Doc</h2>
               <p>Can&apos;t find an answer? Ask a medical professional directly!</p>
-            </Col>
-          </Row>
-        </Card>
-      </Col>
-    </Container>
-    <Container fluid className="color4 p-4 d-flex justify-content-center align-items-center">
-      <Col className="col-10">
-        <Card className="rounded-3 p-5 my-5">
-          <Row className="align-middle text-center my-5">
-            <Col className="">
-              <h2>Our Mission</h2>
-              <br />
-              <p>
-                Our mission is to empower individuals to take control of their health by providing accessible,
-                accurate, and supportive medical resources. Through innovative tools and educational content,
-                we aim to promote well-being, self-care, and informed decision-making for a healthier community.
-              </p>
             </Col>
           </Row>
         </Card>
