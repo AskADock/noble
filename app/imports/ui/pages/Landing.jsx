2dfc9bb3525269f900426f53b50377bf6d00557e
thomasarivera <thomas5info@gmail.com>
2024-11-14 12:09:30 -1000

    update feature section

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -17,17 +17,17 @@ const Landing = () => (
           <Row className="py-5 g-3 justify-content-center text-shadow">
             <Col xs={12} sm="auto" className="text-center">
               <Button className="rounded-3" variant="outline-light" size="lg" href="/faq">
-                FAQ
+                <strong>FAQ</strong>
               </Button>
             </Col>
             <Col xs={12} sm="auto" className="text-center">
               <Button className="rounded-3" variant="outline-light" size="lg" href="/question-compass">
-                Question Compass
+                <strong>Question Compass</strong>
               </Button>
             </Col>
             <Col xs={12} sm="auto" className="text-center">
               <Button className="rounded-3" variant="outline-light" size="lg" href="/ask-a-doc">
-                Ask A Doc
+                <strong>Ask A Doc</strong>
               </Button>
             </Col>
           </Row>
@@ -38,7 +38,7 @@ const Landing = () => (
     {/* Features Section */}
     <Container fluid className="features-section p-0 justify-content-center align-items-center">
       <Container>
-        <Row className="text-center py-3 text-dark-blue">
+        <Row className="text-center py-3 mt-3 text-dark-blue">
           <h1 className="mb-3 text-dark-blue">Your Private Health Resource</h1>
           <h3 className="mb-3">Whether it&apos;s a minor query or a complex issue, you&apos;ll find the guidance you need.</h3>
         </Row>
@@ -48,14 +48,14 @@ const Landing = () => (
             <Card className="feature-card rounded-4 p-3 shadow-sm">
               <Card.Img variant="top" src="/images/landing/FAQ.png" className="feature-image" />
               <Card.Body className="text-center">
-                <h2 className="features-title">Find health-related questions </h2>
+                <h2 className="features-title">Find health-related questions</h2>
                 <p className="feature-text">
                   Search through medical questions written by people like you.
                 </p>
-                <Button variant="primary" href="/faq" className="feature-button">
-                  FAQ
-                </Button>
               </Card.Body>
+              <Button variant="primary" href="/faq" className="feature-button">
+                <strong>FAQ</strong>
+              </Button>
             </Card>
           </Col>
 
@@ -69,10 +69,10 @@ const Landing = () => (
                   Chat with our AI assistant and find relevant information to your medical
                   questions.
                 </p>
-                <Button variant="primary" href="/question-compass" className="feature-button">
-                  Question Compass
-                </Button>
               </Card.Body>
+              <Button variant="outline-dark" href="/question-compass" className="feature-button">
+                <strong>Question Compass</strong>
+              </Button>
             </Card>
           </Col>
 
@@ -83,57 +83,59 @@ const Landing = () => (
               <Card.Body className="text-center">
                 <h2 className="features-title">Ask A Doc</h2>
                 <p className="feature-text">
-                  Can’t find an answer? Anonymously Ask A Doc questions. Answeres can be found in the FAQ
+                  Can’t find an answer? Anonymously ask questions. Answers found in the FAQ
                   page.
                 </p>
-                <Button variant="primary" href="/ask-a-doc">Ask a Doc</Button>
               </Card.Body>
+              <Button variant="dark" href="/ask-a-doc" className="feature-button">
+                <strong>Ask a Doc</strong>
+              </Button>
             </Card>
           </Col>
         </Row>
       </Container>
+    </Container>
 
-      <Container fluid className="color3">
-        <Container className="my-4">
-          <Card className="rounded-4 p-2 text-center">
-            <Card.Body>
-              <Row className="my-4">
-                <Col sm={12} md={6}>
-                  <Image src="images/landing/MedGrp.jpg" alt="Medical pic" width="85%" className="rounded-4" />
-                </Col>
-                <Col sm={12} md={6}>
-                  <h2 style={{ color: 'rgba(15.81, 18.83, 106.57, 0.98)', fontSize: 40, fontWeight: 700 }}>
-                    Stay Proactive
-                  </h2>
-                  <p style={{ color: 'black', fontSize: 20, fontWeight: 600, wordWrap: 'break-word' }}>
-                    Regular health assessments and preventive care ensure that service members meet medical and
-                    dental standards essential for fitness for duty and deployment readiness. The Military Health
-                    System emphasizes that maintaining medical and dental preventive care fitness is vital for
-                    sustaining health and wellness, facilitating restoration, and meeting medical readiness
-                    standards.
-                  </p>
-                </Col>
-              </Row>
-              <Row className="my-4">
-                <Col sm={12} md={6}>
-                  <h2 style={{ color: 'rgba(15.81, 18.83, 106.57, 0.98)', fontSize: 36, fontWeight: 700 }}>
-                    Stay Private
-                  </h2>
-                  <p style={{ color: 'black', fontSize: 20, fontWeight: 600, wordWrap: 'break-word' }}>
-                    Our mission is to empower individuals to take control of their health by providing accessible,
-                    accurate, and supportive medical resources. Through innovative tools and educational content,
-                    we aim to promote well-being, self-care, and informed decision-making for a healthier
-                    community. Your information will never be shared, and your identity will always be hidden and
-                    secure.
-                  </p>
-                </Col>
-                <Col sm={12} md={6}>
-                  <Image src="images/landing/supply-drop.jpg" alt="Medical pic" width="85%" className="rounded-4" />
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Container>
+    <Container fluid className="color3">
+      <Container className="my-4">
+        <Card className="rounded-4 p-2 text-center">
+          <Card.Body>
+            <Row className="my-4">
+              <Col sm={12} md={6}>
+                <Image src="images/landing/MedGrp.jpg" alt="Medical pic" width="85%" className="rounded-4" />
+              </Col>
+              <Col sm={12} md={6}>
+                <h2 style={{ color: 'rgba(15.81, 18.83, 106.57, 0.98)', fontSize: 40, fontWeight: 700 }}>
+                  Stay Proactive
+                </h2>
+                <p style={{ color: 'black', fontSize: 20, fontWeight: 600, wordWrap: 'break-word' }}>
+                  Regular health assessments and preventive care ensure that service members meet medical and
+                  dental standards essential for fitness for duty and deployment readiness. The Military Health
+                  System emphasizes that maintaining medical and dental preventive care fitness is vital for
+                  sustaining health and wellness, facilitating restoration, and meeting medical readiness
+                  standards.
+                </p>
+              </Col>
+            </Row>
+            <Row className="my-4">
+              <Col sm={12} md={6}>
+                <h2 style={{ color: 'rgba(15.81, 18.83, 106.57, 0.98)', fontSize: 36, fontWeight: 700 }}>
+                  Stay Private
+                </h2>
+                <p style={{ color: 'black', fontSize: 20, fontWeight: 600, wordWrap: 'break-word' }}>
+                  Our mission is to empower individuals to take control of their health by providing accessible,
+                  accurate, and supportive medical resources. Through innovative tools and educational content,
+                  we aim to promote well-being, self-care, and informed decision-making for a healthier
+                  community. Your information will never be shared, and your identity will always be hidden and
+                  secure.
+                </p>
+              </Col>
+              <Col sm={12} md={6}>
+                <Image src="images/landing/supply-drop.jpg" alt="Medical pic" width="85%" className="rounded-4" />
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
         {/* <Container fluid className="color2 p-4 d-flex justify-content-center align-items-center"> */}
         {/*  <Row className="justify-content-end align-content-end"> */}
         {/*    <Col xs={12} lg={4} className="d-flex justify-content-center"> */}
