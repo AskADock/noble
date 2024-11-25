27988311f161cf40dd84c20e72b038ae18dbc212
thomasarivera <thomas5info@gmail.com>
2024-11-24 15:24:36 -1000

    update flyer generator

--- app/imports/ui/pages/FlyerManagement.jsx
+++ app/imports/ui/pages/FlyerManagement.jsx
@@ -34,10 +34,20 @@ const FlyerManagement = () => {
       return;
     }
     const printContents = printRef.current.innerHTML;
-    const originalContents = document.body.innerHTML;
-    document.body.innerHTML = printContents;
-    window.print();
-    document.body.innerHTML = originalContents;
+    const iframe = document.createElement('iframe');
+    iframe.style.position = 'absolute';
+    iframe.style.width = '0';
+    iframe.style.height = '0';
+    iframe.style.border = 'none';
+    document.body.appendChild(iframe);
+    const doc = iframe.contentWindow.document;
+    doc.open();
+    doc.write('<html><head><title>Print</title><style></style></head><body>');
+    doc.write(`<div style="text-align: center;">${printContents}</div>`);
+    doc.close();
+    iframe.contentWindow.focus();
+    iframe.contentWindow.print();
+    document.body.removeChild(iframe);
   };
 
   return (ready ? (
@@ -84,10 +94,12 @@ const FlyerManagement = () => {
           <Col sm={12} md={8} className="py-2">
             {/* Printable Area */}
             <Card className="rounded-4 p-2">
-              <div ref={printRef} className="printable">
+              <div ref={printRef}>
                 <Row className="py-4">
                   <Col className="justify-content-center text-center">
-                    <h1>Noble</h1>
+                    <h1 style={{ fontSize: '40px' }}>
+                      Noble
+                    </h1>
                     <p style={{ fontSize: '20px' }}>
                       Prepared for Tomorrow
                     </p>
@@ -103,7 +115,7 @@ const FlyerManagement = () => {
                     <h2>
                       No Login Required
                     </h2>
-                    <Image src="/images/154_Logo.png" alt="Noble Logo" width="50%" style={{ maxWidth: '250px' }} />
+                    <Image src="/images/154_Logo.png" alt="Noble Logo" width="50%" style={{ maxWidth: '225px' }} />
                   </Col>
                 </Row>
               </div>
